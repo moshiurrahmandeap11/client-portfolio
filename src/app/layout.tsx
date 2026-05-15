aaeb406fcf6c01a00121a692ffc5473051e65ac4
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-05-15 19:04:42 +0600

    add google adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -85,6 +86,8 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7757119469762225"
+          crossOrigin="anonymous"></Script>
         <div className="w-full max-w-10/12 mx-auto">
           <main className="min-h-screen flex gap-6">
             {/* Content */}
